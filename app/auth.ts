42eba4a175332e161a4dc9a3bb5b7f4a0dbe84b9
Jvzqz13 <j.vzqz13@gmail.com>
2024-04-06 22:57:32 -0400

    auth with authjs

--- app/auth.ts
+++ app/auth.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from 'next-auth/providers/google';
 
-export const { handlers:{GET, POST}, auth, signIn, signOut } = NextAuth({
+export const { handlers:{GET, POST }, auth, signIn, signOut } = NextAuth({
         providers:[GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
